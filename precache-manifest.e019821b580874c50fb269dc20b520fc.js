3f833526d9eeb35f47d6d5befe4aca7905b675e7
Kade <sugiantara.igk@gmail.com>
2020-04-26 14:45:01 +0700

    initial commit

---
+++ precache-manifest.e019821b580874c50fb269dc20b520fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "661e2cad35424faeb5a738d0d17c97c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f06d5469f8cfef88b02",
+    "url": "/static/css/main.8d521b38.chunk.css"
+  },
+  {
+    "revision": "06536b32b51e94b46c63",
+    "url": "/static/js/2.36ef8114.chunk.js"
+  },
+  {
+    "revision": "8f06d5469f8cfef88b02",
+    "url": "/static/js/main.5e762e0a.chunk.js"
+  },
+  {
+    "revision": "723f5b98b96143a3de6c",
+    "url": "/static/js/runtime-main.461578b1.js"
+  }
+]);
